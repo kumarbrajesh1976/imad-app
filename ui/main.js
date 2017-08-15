ef4345f964649f3cb872e8a06e7b365bf926bd4c
kumarbrajesh1976 <30657090+kumarbrajesh1976@users.noreply.github.com>
2017-08-15 19:36:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
-var button=document.getElementById('counter');
-
+//Submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
 button.onclick=function(){
     //create a request object
     var request=new XMLHttpRequest();
@@ -9,31 +11,23 @@ button.onclick=function(){
         if (request.readyState == XMLHttpRequest.DONE){
             //action
             if (request.status==200){
-               var counter= request.responseText;
-               var span=document.getElementById('count');
-               span.innerHTML=counter.toString();
-        }
-        //not done
-    }
-    
-};
- //make the request
- request.open('GET','http://kumarbrajesh1976.imad.hasura-app.io/counter',true);
- request.send(null);
- 
-};
-
-//Submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
-    
-    var names=['name1','name2','name3','name4'];
+               var names = request.responseText;
+               names = JSON.parse(names);
     var list='';
     for (var i=0;i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+        }
+        //not done
+    }
+    
 };
+ //make the request
+ request.open('GET','http://kumarbrajesh1976.imad.hasura-app.io/submit-name?name='+name,true);
+ request.send(null);
+ 
+};
+
+
