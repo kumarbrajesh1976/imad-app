b0576b88195aada91c6777b78b42ba544e377cc0
kumarbrajesh1976 <30657090+kumarbrajesh1976@users.noreply.github.com>
2017-09-01 12:48:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,27 +6,32 @@ submit.onclick=function(){
     
     //capture the response and store it in a variable
     request.onreadystatechange=function(){
-        if (request.readyState == XMLHttpRequest.DONE){
+        if (request.readyState === XMLHttpRequest.DONE){
             //action
-            if (request.status==200){
-               var names = request.responseText;
-               names = JSON.parse(names);
-    var list='';
-    for (var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+            if (request.status === 200){
+              console.log('User logged in');
+              alert('logged in successfully');
+        } else {
+            if (request.status===403){
+                alert('Username or password is incorrect');
+            } else { 
+                if  (request.status === 500) {
+                alert('Something went wrong on the server');
+                }
+            }
         }
         //not done
     }
     
 };
  //make the request
- var nameInput=document.getElementById('name');
-var name=nameInput.value;
- request.open('GET','http://kumarbrajesh1976.imad.hasura-app.io/submit-name?name='+name,true);
- request.send(null);
+ var username=document.getElementById('username').value;
+ var password=document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
+ request.open('POST','http://kumarbrajesh1976.imad.hasura-app.io/login',true);
+ request.setRequestHeader('Cotent-Type','application/json');
+ request.send(JSON.stringify({username: username, password: password}));
  
 };
 
