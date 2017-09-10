63b62caa2d9c7e7013205e3b68f76ddb3c602e94
kumarbrajesh1976 <30657090+kumarbrajesh1976@users.noreply.github.com>
2017-09-10 17:07:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     user: 'kumarbrajesh1976',
     database: 'kumarbrajesh1976',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: 5432,
     password: process.env.DB_PASSWORD
 };
 
